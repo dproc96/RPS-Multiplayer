3bc3ab64ed124c7c32118f1e9ffafeea112c761f
Dan Proctor <dproc96@gmail.com>
2019-07-06 09:54:54 -0400

    Added log out and waiting queue functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,7 +27,13 @@ var Account = {
                     Account.setActiveUser(snapshotValue);
                     Account.writeLoggedInState()
                 }
+                else {
+                    Account.writeLogInModal()
+                }
             })
+        }
+        else {
+            Account.writeLogInModal()
         }
     },
     createAccount(user) {
@@ -55,6 +61,12 @@ var Account = {
             }
         })
     },
+    logOut() {
+        localStorage.clear();
+        Account.activeUser = null;
+        $("#status").html("<p>You are not logged in</p>");
+        Account.writeLogInModal();
+    },
     pullUser() {
         database.ref(`/${this.activeUser.username}`).on("value", function(snapshot) {
             let snapshotValue = snapshot.val();
@@ -76,11 +88,103 @@ var Account = {
         console.log(this.activeUser);
     },
     writeLoggedInState() {
-        $("#status").text(`You are logged in as ${this.activeUser.username}`)
+        let statusDiv = $("#status");
+        statusDiv.empty();
+        let p = $("<p>");
+        let button = $("<button>");
+        p.text(`Welcome ${this.activeUser.username}!`)
+        statusDiv.append(p);
+        button.text("Log Out");
+        button.attr("id", "log-out-button");
+        statusDiv.append(button);
+        $("#overlay").empty();
+    },
+    writeLogInModal() {
+        let overlay = $("#overlay");
+        let logInDiv = $("<div>");
+        let logInH3 = $("<h3>");
+        let logInUsername = $("<input>");
+        let logInPassword = $("<input>");
+        let logInButton = $("<button>");
+        logInH3.text("Log In");
+        logInDiv.append(logInH3);
+        logInUsername.attr("id", "log-in-username");
+        logInUsername.attr("type", "text");
+        logInUsername.attr("placeholder", "Username");
+        logInDiv.append(logInUsername);
+        logInPassword.attr("id", "log-in-password");
+        logInPassword.attr("type", "password");
+        logInPassword.attr("placeholder", "Password");
+        logInDiv.append(logInPassword);
+        logInButton.text("Log In");
+        logInButton.attr("id", "log-in-button");
+        logInDiv.append(logInButton);
+        let createAccountDiv = $("<div>");
+        let createAccountH3 = $("<h3>");
+        let createAccountUsername = $("<input>");
+        let createAccountPassword = $("<input>");
+        let createAccountPasswordReentered = $("<input>");
+        let createAccountButton = $("<button>");
+        createAccountH3.text("Create Account");
+        createAccountDiv.append(createAccountH3);
+        createAccountUsername.attr("id", "create-account-username");
+        createAccountUsername.attr("type", "text");
+        createAccountUsername.attr("placeholder", "Username");
+        createAccountDiv.append(createAccountUsername);
+        createAccountPassword.attr("id", "create-account-password");
+        createAccountPassword.attr("type", "password");
+        createAccountPassword.attr("placeholder", "Password");
+        createAccountDiv.append(createAccountPassword);
+        createAccountPasswordReentered.attr("id", "create-account-password-reentered");
+        createAccountPasswordReentered.attr("type", "password");
+        createAccountPasswordReentered.attr("placeholder", "Password");
+        createAccountDiv.append(createAccountPasswordReentered);
+        createAccountButton.text("Create Account");
+        createAccountButton.attr("id", "create-account-button");
+        createAccountDiv.append(createAccountButton);
+        overlay.append(logInDiv);
+        overlay.append(createAccountDiv);
     }
 }
 
-$("#create-account-button").click(function() {
+var Queue = {
+    addToQueue(username) {
+        var queue
+        database.ref("/queue").on("value", function(snapshot) {
+            queue = snapshot.val().queue.split(",");
+        })
+        setTimeout(function() {
+            queue.push(username);
+            queue = queue.join(",")
+            database.ref("/queue").set({queue})
+        }, 1000)
+    },
+    checkQueue() {
+        var queue
+        database.ref("/queue").on("value", function (snapshot) {
+            queue = snapshot.val().queue.split(",");
+        })
+        setTimeout(function() {
+            if (queue.length > 1) {
+                let partner = queue[1]
+                queue.splice(1, 1)
+                queue = queue.join(",")
+                database.ref("/queue").set({ queue })
+                console.log(partner)
+                //Start game function
+                return;
+            }
+            else {
+                //Waiting function
+                Queue.addToQueue(Account.activeUser.username)
+                return;
+            }
+        }, 1000)
+    }
+}
+
+Queue.checkQueue()
+$(document).on("click", "#create-account-button", function() {
     let user = {
         username: $("#create-account-username").val(),
         password: $("#create-account-password").val(),
@@ -96,11 +200,16 @@ $("#create-account-button").click(function() {
     }
 })
 
-$("#log-in-button").click(function() {
+$(document).on("click", "#log-in-button", function() {
     let username = $("#log-in-username").val();
     let password = $("#log-in-password").val();
     Account.logIn(username, password);
 })
+
+$(document).on("click", "#log-out-button", function() {
+    Account.logOut();
+})
+
 
 Account.checkIfLoggedIn()
 //Method to execute after a user is logged in
